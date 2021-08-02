c32644001a1a2f507a25ca9ec8c35fd7716b1004
Mati <mateusz.l.jusiak@gmail.com>
2021-08-02 20:58:15 +0200

    Added Sign in / Sign up component with css

--- frontendmicro/src/components/MainMicroserviceApp.jsx
+++ frontendmicro/src/components/MainMicroserviceApp.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import Home from './HomePage'
+import SignInSignUp from './SignInSignUp'
 import './../css/bootstrap.min.css'
 import './../css/style.css'
 
@@ -10,9 +11,13 @@ class MainMicroserviceApp extends Component {
             <div className="MainMicroserviceApp">
                 <Router>
                         <HeaderComponent/>
+                        <div className="bg">
                         <Switch>
                             <Route path="/" exact component={Home}/>
+                            <Route path="/signinsignup" exact component={SignInSignUp}/>
+                            <Route path="/" exact component={Home}/>
                         </Switch>
+                        </div>
                 </Router>
             </div>
         );
@@ -23,13 +28,13 @@ class HeaderComponent extends Component {
     render() {
         return (
             <header>
-                <nav className="navbar navbar-expand-md navbar-light bg-light">
+                <nav className="navbar navbar-expand navbar-light bg-light">
                     <ul className="navbar-nav">
                         <li><Link className="nav-link" to="/">Home</Link></li>
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
-                        <li><Link className="nav-link" to="/">Login</Link></li>
-                        <li><Link className="nav-link" to="/">Logout</Link></li>
+                        <li><Link className="nav-link" to="/signinsignup">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
                     </ul>
                 </nav>
             </header>
